0bae8126af394bda2e90941774289f8183426975
Hack Reactor Students <bgraeber@gmail.com>
2014-03-04 10:18:20 -0800

    Small tweak

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -2,7 +2,7 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-  Dancer.call(this,top, left, timeBetweenSteps);
+  Dancer.call(this, top, left, timeBetweenSteps);
   var oldStep = this.step;
 
   this.step = function(){
